67209962e55512041ae00f2cb1282f168163cad5
MatToufoutu <mattoufootu@gmail.com>
2016-11-22 08:36:30 +0100

    update keywords, compatible python versions, and dev stage

--- setup.py
+++ setup.py
@@ -21,21 +21,21 @@ with open(os.path.join(CURDIR, 'requirements.txt')) as ifile:
 setup(
     name='Passtis',
     version=passtis.__version__,
-    description='Command line password manager',
+    description=' GnuPG-based command line password manager',
     long_description=README,
     author='Mathieu D. (MatToufoutu)',
     author_email='mattoufootu@gmail.com',
     url='TBD',
     zip_safe=False,
     license='GPLv3',
-    keywords='passwords password-manager',
+    keywords='pass passwd password password-manager gpg gnupg',
     install_requires=REQUIREMENTS,
     py_modules=['passtis'],
     entry_points={
         'console_scripts': ['passtis = passtis:main']
     },
     classifiers=[
-        'Development Status :: 1 - Planning',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: End Users/Desktop',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
@@ -44,7 +44,6 @@ setup(
         'Operating System :: MacOS',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3',
     ]
 )
